ca17db8d99aa7bcb86ffb22a7e2faa9d88f33aad
GeethaKS <GeethaKS@SGEETHA-P14S>
2024-08-27 14:19:16 +0530

    Add project files.

---
+++ TodoApi1/Data/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>().ToTable("Todos");
+        }
+    }
+}
